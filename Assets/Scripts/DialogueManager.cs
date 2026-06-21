0b4613fea5323336557f6238c90dfdddcab1602a
WilmerNilsson <a23wilni@student.his.se>
2026-06-21 16:23:07 +0200

    Dialogue + TMP
    Text mesh pro

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] private GameObject dialogueBox;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    private string[] sentences;
+    private string currentSentence;
+    private int sentenceIndex;
+    public char divider = '|';
+    public float typeDelay = 0.2f;
+    private bool isTyping;
+
+    private bool HasNextSentence
+    {
+        get
+        {
+            return sentenceIndex < sentences.Length - 1;
+        }
+    }
+
+    private void Awake()
+    {
+        dialogueBox.SetActive(false);
+    }
+
+    public void StartDialogue(TextAsset textAsset)
+    {
+        dialogueBox.SetActive(true);
+        dialogueText.text = "";
+        sentences = textAsset.text.Split(divider);
+        sentenceIndex = 0;
+        currentSentence = sentences[sentenceIndex];
+        StartCoroutine(TypeSentence(currentSentence));
+    }
+
+    private IEnumerator TypeSentence(string sentence)
+    {
+        bool richText = false;
+        isTyping = true;
+        dialogueText.text = "";
+        foreach (char letter in sentence)
+        {
+            if (letter == '<')
+            {
+                richText = true;
+            }
+            else if (letter == '>')
+            {
+                richText = false;
+            }
+            dialogueText.text += letter;
+            if (!richText)
+            {
+                yield return new WaitForSeconds(typeDelay);
+            }
+        }
+        isTyping = false;
+    }
+
+    public void AdvanceDialogue()
+    {
+        if (isTyping)
+        {
+            StopAllCoroutines();
+            dialogueText.text = currentSentence;
+            isTyping = false;
+            return;
+        }
+
+        if (HasNextSentence)
+        {
+            sentenceIndex++;
+            currentSentence = sentences[sentenceIndex];
+            StopAllCoroutines();
+            StartCoroutine(TypeSentence(sentences[sentenceIndex]));
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    private void EndDialogue()
+    {
+        dialogueBox.SetActive(false);
+        StopAllCoroutines();
+        isTyping = false;
+    }
+}
